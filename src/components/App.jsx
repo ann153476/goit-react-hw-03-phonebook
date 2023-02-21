cc9b4db86945cf9aa347e8a31fc088038c95c0b9
Anka <103377387+ann153476@users.noreply.github.com>
2023-02-21 14:03:44 +0200

    /

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,77 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+import { nanoid } from 'nanoid';
+
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './filter/Filter';
+import Notification from './Notification/Notification';
+
+const message = 'There is no contacts';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '4591256' },
+      { id: 'id-2', name: 'Hermione Kline', number: '4438912' },
+      { id: 'id-3', name: 'Eden Clements', number: '6451779' },
+      { id: 'id-4', name: 'Annie Copeland', number: '2279126' },
+    ],
+    filter: '',
+  };
+  addContact = ({ name, number }) => {
+    const normalizedName = name.toLowerCase();
+
+    this.state.contacts.forEach(el => {
+      if (el.name.toLowerCase() === normalizedName) {
+        alert(`${name} is already in contacts`);
+        return;
+      }
+    });
+    const contact = {
+      id: nanoid(),
+      name: name,
+      number: number,
+    };
+
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, contact],
+    }));
+  };
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+  deleteContact = todoId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== todoId),
+    }));
+  };
+  render() {
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <Filter value={filter} onChange={this.changeFilter}></Filter>
+        {visibleContacts.length === 0 ? (
+          <Notification message={message} />
+        ) : (
+          <ContactList
+            contacts={visibleContacts}
+            onDeleteContact={this.deleteContact}
+          />
+        )}
+      </>
+    );
+  }
+}
+
+export default App;
